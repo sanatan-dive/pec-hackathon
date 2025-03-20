8ff11acf8389435b63dd6505f57656b6d90c25d6
satvikpathak007 <satvikpathak007@gmail.com>
2025-03-21 05:06:32 +0530

    mazak hackathon

---
+++ src/app/networks/page.tsx
@@ -0,0 +1,13 @@
+
+import NetworksPage from '@/components/network'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <NetworksPage/>
+    </div>
+  )
+}
+
+export default page
